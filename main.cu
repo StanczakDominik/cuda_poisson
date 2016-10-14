8d46b960bbeb153fef57e61bb3fee72e701e8f62
Dominik Stańczak <stanczakdominik@gmail.com>
2016-10-14 21:56:55 +0200

    iteratePoisson kernel

--- main.cu
+++ main.cu
@@ -4,9 +4,29 @@
 #define DX (1./(float)NX)
 #define DY (1./(float)NY)
 
+// Solves the Poisson equation via the Jacobi Method
+// $$\nabla^2 \phi = f$$
+
+__global__ void iteratePoisson(float* d_source, float* d_V1, float* d_V2)
+{
+    int i = blockIdx.x * blockSize.x + threadIdx.x;
+    int j = blockIdx.y * blockSize.y + threadIdx.y;
+    int n = j * NX + i;
+    if ((i>0) && (i < NX-1) && (j>0) && (j < NY-1)) { //TODO: see what can be done about boundaries
+        int n_top = (j-1) * NX + i;
+        int n_bot = (j+1) * NX + i;
+        int n_left = j * NX + (i-1);
+        int n_right = j * NX + (i+1);
+        //TODO: rewrite above in terms of n?
+
+        d_V1[n] = 0.25f * (d_V2[n_top] + d_V2[n_bot] + d_V2[n_left] + d_V2[n_right] +\
+            d_source[n] * DX * DY;
+    }
+}
+
 int main()
 {
-    float *h_rho = new float[NX*NY];
+    float *h_source = new float[NX*NY];
     float *h_V = new float[NX*NY];
 
     float top_bc = 1;
@@ -33,7 +53,7 @@ int main()
         }
     }
 
-
-
-
+    float *d_source;
+    float *d_V1;
+    float *d_V2;
 }
